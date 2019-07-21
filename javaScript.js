ad19048a1ac73fe8bbfc97b392d4ef826ee969bd
lpg2709 <leonardo@email.com>
2019-07-21 19:15:53 -0300

    Working, but not in FireFox

--- javaScript.js
+++ javaScript.js
@@ -1,13 +1,36 @@
 const fill = document.querySelector('.fill');
-const empties = document.querySelector('.empty');
+const empties = document.querySelectorAll('.empty');
 
 fill.addEventListener('dragstart',dragStart);
 fill.addEventListener('dragend',dragEnd);
-    
+   
+for(const empty of empties){
+    empty.addEventListener('dragover', dargOver);
+    empty.addEventListener('dragenter', dargEnter);
+    empty.addEventListener('dragleave', dargLeave);
+    empty.addEventListener('drop', dargDrop);
+}
+
 function dragStart(){
-    console.log("Start");
+   this.className += "hold";
+   //setTimeout(()=> {this.className = 'invisible';} , 0);
 }
     
 function dragEnd(){
-    console.log("End");
+    this.className = 'fill';
+}
+
+function dargOver(e){
+    e.preventDefoult();
+}
+function dargEnter(e){
+    e.preventDefoult();
+    this.className += 'hovered';    
+}
+function dargLeave(){
+    this.className = 'empty';    
+}
+function dargDrop(){
+    this.className = 'empty';
+    this.append(fill);     
 }
